fb6058c4e927fc802b8fe3899f416c2911276605
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-31 13:10:28 -0300

    Condicionais

---
+++ condicionais/condicional.py
@@ -0,0 +1,25 @@
+# Condicionais
+
+# if
+idade = 18
+if idade >= 18:
+    print('É de maior!')
+
+# if-else
+idade_2 = 15
+if idade_2 >= 18:
+    print('Pode tirar a habilitação')
+else:
+    print('Ainda não pode tirar a habilitação')
+
+# if-elif
+nota = 6.4
+
+if nota >= 9.0:
+    print('Nota excelente')
+elif nota >= 8.9:
+    print('Nota regular')
+elif nota >= 7.0:
+    print('Nota baixa')
+else:
+    print('Reprovado!')
