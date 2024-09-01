bde4cb9be80b09c6ae246c0048a396f1750dd750
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-01 12:05:48 -0300

    Exercícios em loops

--- repeticoes/repeticoes.py
+++ repeticoes/repeticoes.py
@@ -30,3 +30,24 @@ for i in range(10):
 # pass
 for i in range(5):
     pass
+
+# Exercício 1: Escreva um programa que imprima todos os números de 1 a 10 usando um loop for.
+print('-----------Exercício----------------')
+soma = 0
+for nums in range(1, 101):
+    soma += nums
+
+print('Soma de todos os valores:', soma)
+
+# Exercício 2: Escreva um programa que faça uma contagem regressiva a partir de 10 até 1 e, em seguida, imprima "Feliz Ano Novo!" no final. Use um loop while.
+
+contador2 = 10
+
+while contador2 >= 1:
+    print(contador2)
+    contador2 -= 1
+
+# Exercício 3: Escreva um programa que imprima a tabuada de 5 (de 1 a 10) usando um loop for.
+
+for i in range(1, 11):
+    print(i * 5)
